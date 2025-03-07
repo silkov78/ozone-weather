136701b19e2c2f625a040270e12d48f84d28b3b0
silkov78 <piotr.silkov78@gmail.com>
2025-03-07 13:21:47 +0300

    get request to open-meteo

--- app/Services/WeatherApiService.php
+++ app/Services/WeatherApiService.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use GuzzleHttp\Client;
+
 class WeatherApiService
 {
     public function __construct()
     {
-        echo 'Hello weather api!';
+        $client = new Client();
+
+        $url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m';
+
+        $response = $client->request('GET', $url);
+
+        echo $response->getStatusCode();
+        echo $response->getBody();
     }
 }
