af6ebe140291dfe92b3150107222084633c39a58
silkov78 <piotr.silkov78@gmail.com>
2025-03-08 14:47:14 +0300

    WeatherCodeTest

---
+++ tests/Unit/WeatherCodeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\WeatherDataService\Enums\WeatherCode;
+use PHPUnit\Framework\TestCase;
+
+class WeatherCodeTest extends TestCase
+{
+    public function test_conversion_int_code_in_enum(): void
+    {
+        $this->assertSame(
+            WeatherCode::FOG,
+            WeatherCode::fromCode(45)
+        );
+    }
+
+    public function test_conversion_missing_int_code_in_enum(): void
+    {
+        $this->assertSame(
+            WeatherCode::CLEAR_SKY,
+            WeatherCode::fromCode(888)
+        );
+    }
+}
