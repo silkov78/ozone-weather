dd5246cb533cab46362345ec165a2af5c640f0f0
silkov78 <piotr.silkov78@gmail.com>
2025-03-10 18:13:14 +0300

    binding all interfaces in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,15 @@
 
 namespace App\Providers;
 
+use App\Services\WeatherServiceProvider\OpenMeteoProvider;
+use App\Services\WeatherServiceProvider\WeatherProvider;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Validation\Factory as ValidatorInterface;
+use Psr\Http\Message\ResponseInterface;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +19,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(WeatherProvider::class, OpenMeteoProvider::class);
+
+        // Bind Guzzle HTTP client
+        $this->app->bind(ClientInterface::class, function () {
+            return new Client();
+        });
+
+        // Bind Laravel Validator
+        $this->app->bind(ValidatorInterface::class, function ($app) {
+            return $app['validator'];
+        });
+
+        $this->app->bind(ResponseInterface::class, function () {
+            return new Response();
+        });
     }
 
     /**
